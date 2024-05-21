b5fc04fed768a44c9e23a1b6c7e5a67553ab5a48
Hrithik Kedare <kedarehrithik@gmail.com>
2024-05-21 10:10:18 +0530

    input-field-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import './App.css';
-
-
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
 // let role:[number,string];
 // role=[5,"stu"];
 // only number,string allowed and that to in only 2 allowed.
-function App() {
+// function App() {
+//   return (
+//     <div>Hello World
+//     </div>
+//   );
+// }
+
+const App: React.FC = () => {
   return (
-    <div>Hello World
-    </div>
+    
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField />
+      </div>
+
   );
 }
-
 export default App;
